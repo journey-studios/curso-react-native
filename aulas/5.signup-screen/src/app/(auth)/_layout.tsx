5037bcb8ef4e1ac83e275c92f6198efe6cb9bea7
Wallysson <wally.nm@gmail.com>
2024-09-01 14:28:46 -0300

    Fixes missing screen-option config

--- aulas/5.signup-screen/src/app/(auth)/_layout.tsx
+++ aulas/5.signup-screen/src/app/(auth)/_layout.tsx
@@ -2,7 +2,11 @@ import { Stack } from "expo-router"
 
 const AuthLayout = () => {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
       <Stack.Screen name="login" />
       <Stack.Screen name="signup" />
     </Stack>
